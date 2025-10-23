21ee391ad4e2c9e513251c0c13aaf4d24e9aee40
Alexandr884 <bmwe34alexandr@icloud.com>
2025-10-23 22:10:57 +0500

    finished

--- src/components/notes.js
+++ src/components/notes.js
@@ -3,11 +3,11 @@ import React from "react"
  export default function Notes ({todo}){
 
    return ( 
-      <div>
+      <div className="notes">
          {
             todo.map(item => ( 
                <div className="note" key={item.id}>
-               {item.title}
+               <p className="title">{item.title}</p>
                </div>
             ))
          } 
